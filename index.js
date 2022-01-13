57c990ba0ca628b61a934ef433c81935795e634a
Kale Leach <kleach1212@gmail.com>
2022-01-12 20:58:55 -0800

    commit tree c4cca499037445b87f21e2e7a0f49301362b248e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = (name) => drivers.slice(0,2);
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = (name) => drivers.slice(2,5)
+returnLastTwoDrivers();
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return () => num * 5;
+}
+
+const fareDoubler = (num) => num * 2;
+
+const fareTripler = (num) => num * 3;
+
+function selectDifferentDrivers (drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+} 
